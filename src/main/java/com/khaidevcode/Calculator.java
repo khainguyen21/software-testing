b243f59eaef67cae5b79f2a8fc0ccb44c84d70d0
Khai Nguyen <khainguyen2004@gmail.com>
2025-07-12 19:50:30 -0700

    first commit

--- src/main/java/com/khaidevcode/Calculator.java
+++ src/main/java/com/khaidevcode/Calculator.java
@@ -1,4 +1,7 @@
 package com.khaidevcode;
 
 public class Calculator {
+    public <T extends Number> int add(T a, T b) {
+        return a.intValue() + b.intValue();
+    }
 }
--- src/test/java/com/khaidevcode/CalculatorTest.java
+++ src/test/java/com/khaidevcode/CalculatorTest.java
@@ -1,4 +1,14 @@
 package com.khaidevcode;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CalculatorTest {
+    @Test
+    void canAddNumbers() {
+        var calculator = new Calculator();
+        var result = calculator.add(3, 3);
+        assertEquals(6, result);
+    }
 }
