fa417d46d7f866f1edd1b21be07072bb3400373a
Khai Nguyen <khainguyen2004@gmail.com>
2025-07-13 11:07:04 -0700

    Practice software-testing using Junit

--- src/test/java/com/khaidevcode/CalculatorTest.java
+++ src/test/java/com/khaidevcode/CalculatorTest.java
@@ -3,12 +3,33 @@ package com.khaidevcode;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.*;
+
 
 public class CalculatorTest {
+    private final Calculator underTest = new Calculator();
+
     @Test
     void canAddNumbers() {
-        var calculator = new Calculator();
-        var result = calculator.add(3, 3);
-        assertEquals(6, result);
+        // 1 - given
+        var number1 = 3;
+        var number2 = 3;
+
+        // 2 - when
+        var actual = underTest.add(number1, number2);
+
+        // 3 - then
+//        assertEquals(6, result);
+        var expected = 6;
+        assertThat(actual).isEqualTo(expected);
+
+    }
+
+
+    @Test
+    void name() {
+        //given
+        //when
+        //then
     }
 }
